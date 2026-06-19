373cc5e7cde1021f298a8e61ac2096a543c10674
fatihabdrhmn <fatih.rahman@pgnsolution.co.id>
2026-06-19 18:51:14 +0700

    Configure backend to serve frontend (monolith)

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 import './src/config/db.js'  
 import authRoutes from './src/routes/auth.routes.js'
 import userRoutes from './src/routes/user.routes.js'
@@ -21,6 +27,16 @@ app.use('/api/users', userRoutes)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server PKRS-ABK berjalan!' })
+})
+
+// Melayani file frontend (Web)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('*', (req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(404).json({ message: 'API Route Not Found' })
+  }
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 if (process.env.VERCEL !== '1') {
